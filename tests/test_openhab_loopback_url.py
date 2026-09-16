845a1f6ee58a3ab763a9d0280cd7323596066476
Sat <792024+santyr@users.noreply.github.com>
2026-09-16 09:01:54 -0600

    test: cover OpenHAB loopback URL exception

---
+++ tests/test_openhab_loopback_url.py
@@ -0,0 +1,51 @@
+import socket
+
+import pytest
+
+from lnbits.extensions.lightning_goats.services import url_validation
+from lnbits.extensions.lightning_goats.services.url_validation import (
+    OutboundURLPolicyError,
+    ensure_outbound_url_allowed,
+    validate_outbound_url,
+)
+
+
+def test_openhab_allows_ipv4_loopback():
+    assert (
+        ensure_outbound_url_allowed("http://127.0.0.1:8080", "OpenHAB URL")
+        == "http://127.0.0.1:8080"
+    )
+
+
+def test_openhab_allows_localhost_and_ipv6_loopback():
+    assert (
+        ensure_outbound_url_allowed("http://localhost:8080", "OpenHAB URL")
+        == "http://localhost:8080"
+    )
+    assert (
+        ensure_outbound_url_allowed("http://[::1]:8080", "OpenHAB URL")
+        == "http://[::1]:8080"
+    )
+
+
+def test_generic_policy_still_rejects_loopback():
+    with pytest.raises(OutboundURLPolicyError):
+        validate_outbound_url("http://127.0.0.1:8080")
+
+
+def test_non_openhab_integration_still_rejects_loopback():
+    with pytest.raises(OutboundURLPolicyError):
+        ensure_outbound_url_allowed(
+            "http://127.0.0.1:5000/get_received_data",
+            "weather station URL",
+        )
+
+
+def test_openhab_does_not_allow_dns_name_that_resolves_to_loopback(monkeypatch):
+    def fake_getaddrinfo(host, *args, **kwargs):
+        return [(socket.AF_INET, None, None, "", ("127.0.0.1", 0))]
+
+    monkeypatch.setattr(url_validation.socket, "getaddrinfo", fake_getaddrinfo)
+
+    with pytest.raises(OutboundURLPolicyError):
+        ensure_outbound_url_allowed("http://sneaky.example.com:8080", "OpenHAB URL")
